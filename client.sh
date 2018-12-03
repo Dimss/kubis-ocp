d4e4a4c5b656db0ebd4692c3794cf43230e64473
Dmitry Kartsev <dkartsev@redhat.com>
2018-12-03 10:02:46 +0200

    update structure

--- client.sh
+++ client.sh
@@ -1,2 +1,2 @@
-curl  -s -H "X-APP-USER: user1" http://kubis-gateway/metadata  | jq -C .
+curl  -v -s -H "X-APP-USER: user1" http://kubis-gateway/metadata  | jq -C .
 #curl  -s -H "X-APP-USER: user1" http://kubis-gateway/version  | jq -C .
