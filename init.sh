d908a65572044fa0b916fe8b00b00e29aaf73872
Dmitry Kartsev <dkartsev@redhat.com>
2018-12-04 09:14:31 +0200

    update init script

--- init.sh
+++ init.sh
@@ -1,22 +1,24 @@
+#!/bin/bash
 # Create  project, update SCC to allow anyuid (root) for Invoy proxy 
 PROJECT_NAME=kubis
 oc new-project ${PROJECT_NAME}
 oc adm policy add-scc-to-user anyuid -z default -n ${PROJECT_NAME}
 oc adm policy add-scc-to-user privileged -z default -n ${PROJECT_NAME}
-
-# Add OpenJDK image stream for S2I java build 
-oc create -f common/openjdk18-is.yaml
-
-# Add telepresence  
-oc create -f common/tel-dc.yaml
-
-echo "Wait 10 sec for openjdk18 IS to become in sync"
+# Add Image streams
+oc create -f init/1-openjdk18-is.yaml
+# Must sleep before continue, openjdk18 image stream should fetch the openjdk18 image metadata 
 sleep 10
-
-# Init Gateway service 
-echo "Init Gateway service"
-cd gw && ./init.sh
-
-# Init REST service
-echo "Init REST service"
-cd ../rest && ./init.sh 
+oc create -f init/2-gw-is.yaml 
+oc create -f init/3-rest-is.yaml 
+# Add Build configs
+oc create -f init/4-gw-bc.yaml
+oc create -f init/5-rest-bc-v0.1.yaml
+oc create -f init/6-rest-bc-v0.3.yaml
+# Build images 
+oc start-build kubis-gw --follow
+oc start-build kubis-rest-0.1 --follow 
+oc start-build kubis-rest-0.3 --follow 
+# Add telepresence  
+oc create -f init/7-dep-tel.yaml
+# Delete finishded build jobs 
+oc get pods | grep build  | awk '{print $1}' | xargs oc delete pod
