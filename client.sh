83d05755f73917a41e00e0677efc0d148172e98f
Dmitry Kartsev <dkartsev@redhat.com>
2018-12-06 15:00:40 +0200

    rename cb

--- client.sh
+++ client.sh
@@ -1,1 +1,1 @@
-curl -s -H "X-APP-USER: kubis-rest-0-3-85c7889969-c9r8m" http://kubis-gw/metadata  | jq -C .
+curl -s -H "X-APP-USER: kubis-rest-0-3-85c7889969-v8f56" http://kubis-gw/metadata | jq -C .
